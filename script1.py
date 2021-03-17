14c284b5899bf57783f7b0ebf6a3d34392ca760f
Georgie Horth <georgiehorth@gmail.com>
2021-03-17 14:15:24 +0000

    Added year and ISBN labels and entry windows

--- script1.py
+++ script1.py
@@ -16,4 +16,18 @@ author_text = StringVar()
 e2 = Entry(window, textvariable=author_text)
 e2.grid(row=0, column=3)
 
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+year_input = StringVar()
+e3 = Entry(window, textvariable=year_input)
+e3.grid(row=1, column=1)
+
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+isbn_input = StringVar()
+e4 = Entry(window, textvariable=isbn_input)
+e4.grid(row=1, column=3)
+
 window.mainloop()
