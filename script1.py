2494671e7e9d0fa454f9d365aed80dc2139ff26e
Georgie Horth <georgiehorth@gmail.com>
2021-03-17 11:23:55 +0000

    Added Author entry box

--- script1.py
+++ script1.py
@@ -9,4 +9,11 @@ title_text = StringVar()
 e1 = Entry(window, textvariable=title_text)
 e1.grid(row=0, column=1)
 
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+
+author_text = StringVar()
+e2 = Entry(window, textvariable=author_text)
+e2.grid(row=0, column=3)
+
 window.mainloop()
