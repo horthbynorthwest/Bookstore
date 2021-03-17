379e70d9a18ce55556736aeaa2d6de60a79e4fd0
Georgie Horth <georgiehorth@gmail.com>
2021-03-17 11:21:55 +0000

    Added Title entry box

--- script1.py
+++ script1.py
@@ -4,4 +4,9 @@ window = Tk()
 
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
+
+title_text = StringVar() 
+e1 = Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
 window.mainloop()
