1efe040553f38fd9228517d6538e66ee1799658c
Georgie Horth <georgiehorth@gmail.com>
2021-03-17 14:59:11 +0000

    Added add and view methods in the backend to talk to db. Currently prints in console

--- backend.py
+++ backend.py
@@ -7,4 +7,21 @@ def connect():
     conn.commit()
     conn.close()
 
+def add(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()   
+    cursor.execute("SELECT * FROM book")
+    all_books = cursor.fetchall()
+    conn.close()
+    return all_books
+
 connect()
+add("The Sea","John Tablet", 1918, 180982)
+print(view())
